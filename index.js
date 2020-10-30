11741e368a0c1b98505adde3d29c0989b29285b9
unknown <peter.lazarov94@gmail.com>
2020-10-30 09:52:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+module.exports.generate = function(size) {
+    let num1 = 0, num1 = 1;
+    let result = [];
+
+    if (size > 0){
+        result.push(num1)
+    }
+    if (size > 1){
+        result.push(num2)
+    }
+
+    let num3;
+    for(let counter = 2; counter < size; counter++)
+    {
+        num3 = num1 + num2;
+        result.push(num3);
+        num1 = num2;
+        num2 = num3;
+    }
+}
